5d89eba01ec7e1152b2c7334040502ddbf4e3344
Marcus Ceradini <mnceradini@my.waketech.edu>
2025-02-07 13:54:23 -0500

    finished everything

---
+++ Challenge 1 Part 2.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: ")) #The input was assigned the value of str instead of int
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are to young to vote")
---
+++ Challenge 2 Part 3.py
@@ -0,0 +1,8 @@
+num = 10
+fact = 1
+
+for i in range(1, num+1):
+    fact *= i #divided the factorial by i instead of multiplying it by i(fact = fact/i). made it fact *= i.
+
+print("The factorials of 10 is")
+print(fact)
